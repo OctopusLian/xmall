549b0245570f29e3856c13ea5ff9442b5c073359
OctopusLian <zoctopus@qq.com>
2022-06-07 18:29:35 +0800

    feature #4：model层增删改查

---
+++ service/product.go
@@ -0,0 +1,176 @@
+/*
+ * @Author: neozhang
+ * @Date: 2022-06-07 18:17:14
+ * @LastEditors: neozhang
+ * @LastEditTime: 2022-06-07 18:28:25
+ * @Description: 请填写简介
+ */
+package service
+
+import (
+	"xmall/e"
+	"xmall/logging"
+	"xmall/model"
+	"xmall/serializer"
+)
+
+type ProductService struct {
+	ID            uint   `form:"id" json:"id"`
+	Name          string `form:"name" json:"name"`
+	CategoryID    int    `form:"category_id" json:"category_id"`
+	Title         string `form:"title" json:"title" binding:"required,min=2,max=100"`
+	Info          string `form:"info" json:"info" binding:"max=1000"`
+	ImgPath       string `form:"img_path" json:"img_path"`
+	Price         string `form:"price" json:"price"`
+	DiscountPrice string `form:"discount_price" json:"discount_price"`
+	Limit         int    `form:"limit" json:"limit"`
+	Start         int    `form:"start" json:"start"`
+}
+
+// Create 创建商品
+func (service *ProductService) Create() serializer.Response {
+	product := model.Product{
+		Name:          service.Name,
+		CategoryID:    service.CategoryID,
+		Title:         service.Title,
+		Info:          service.Info,
+		ImgPath:       service.ImgPath,
+		Price:         service.Price,
+		DiscountPrice: service.DiscountPrice,
+	}
+	code := e.SUCCESS
+
+	err := model.DB.Create(&product).Error
+	if err != nil {
+		logging.Info(err)
+		code = e.ERROR_DATABASE
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+
+	return serializer.Response{
+		Status: code,
+		Msg:    e.GetMsg(code),
+		Data:   serializer.BuildProduct(product),
+	}
+}
+
+// Delete 删除商品
+func (service *ProductService) Delete(id string) serializer.Response {
+	var product model.Product
+	code := e.SUCCESS
+
+	err := model.DB.First(&product, id).Error
+	if err != nil {
+		logging.Info(err)
+		code = e.ERROR_DATABASE
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+
+	err = model.DB.Delete(&product).Error
+	if err != nil {
+		logging.Info(err)
+		code = e.ERROR_DATABASE
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+
+	return serializer.Response{
+		Status: code,
+		Msg:    e.GetMsg(code),
+	}
+}
+
+// 商品列表
+func (service *ProductService) List() serializer.Response {
+	products := []model.Product{}
+
+	total := 0
+	code := e.SUCCESS
+
+	if service.Limit == 0 {
+		service.Limit = 15
+	}
+	if service.CategoryID == 0 {
+		if err := model.DB.Model(model.Product{}).Count(&total).Error; err != nil {
+			logging.Info(err)
+			code = e.ERROR_DATABASE
+			return serializer.Response{
+				Status: code,
+				Msg:    e.GetMsg(code),
+				Error:  err.Error(),
+			}
+		}
+
+		if err := model.DB.Limit(service.Limit).Offset(service.Start).Find(&products).Error; err != nil {
+			logging.Info(err)
+			code = e.ERROR_DATABASE
+			return serializer.Response{
+				Status: code,
+				Msg:    e.GetMsg(code),
+				Error:  err.Error(),
+			}
+		}
+	} else {
+		if err := model.DB.Model(model.Product{}).Where("category_id=?", service.CategoryID).Count(&total).Error; err != nil {
+			logging.Info(err)
+			code = e.ERROR_DATABASE
+			return serializer.Response{
+				Status: code,
+				Msg:    e.GetMsg(code),
+				Error:  err.Error(),
+			}
+		}
+
+		if err := model.DB.Where("category_id=?", service.CategoryID).Limit(service.Limit).Offset(service.Start).Find(&products).Error; err != nil {
+			logging.Info(err)
+			code = e.ERROR_DATABASE
+			return serializer.Response{
+				Status: code,
+				Msg:    e.GetMsg(code),
+				Error:  err.Error(),
+			}
+		}
+	}
+
+	return serializer.BuildListResponse(serializer.BuildProducts(products), uint(total))
+}
+
+// Update 更新商品
+func (service *ProductService) Update() serializer.Response {
+	product := model.Product{
+		Name:          service.Name,
+		CategoryID:    service.CategoryID,
+		Title:         service.Title,
+		Info:          service.Info,
+		ImgPath:       service.ImgPath,
+		Price:         service.Price,
+		DiscountPrice: service.DiscountPrice,
+	}
+	product.ID = service.ID
+	code := e.SUCCESS
+	err := model.DB.Save(&product).Error
+	if err != nil {
+		logging.Info(err)
+		code = e.ERROR_DATABASE
+		return serializer.Response{
+			Status: code,
+			Msg:    e.GetMsg(code),
+			Error:  err.Error(),
+		}
+	}
+	return serializer.Response{
+		Status: code,
+		Msg:    e.GetMsg(code),
+	}
+}
